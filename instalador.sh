fcd1625af47a63e59d456a4754772e9e2cd12ff9
wes <wesleysilva.ti@gmail.com>
2017-08-10 14:05:23 -0300

    atualização de script

--- instalador.sh
+++ instalador.sh
@@ -15,8 +15,7 @@ echo "::Digite o numero e tecle enter ou para cancelar feche no (X)::
 1-Update e upgrade
 2-Instalar sublime text 3
 3-Instalar visual studio code
-4-Instalar Workbench ubuntu 14.04 64bits
-5-Instalar chrome
+4-Instalar chrome
 "
 
 echo 
@@ -54,25 +53,14 @@ then
     umake web visual-studio-code
     echo "--- Fim da instalação do visual studio code  ---"  
 
-
 elif [ "$programas" = "4" ];
-then
-    echo "--- instalando Workbench ubuntu 14.04 64bits ---"
-    sleep 3     
-    wget https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-6.2.5-1ubu1404-amd64.deb -O mysql-workbench-community.deb
-    sudo dpkg -i mysql-workbench-community.deb   
-    sudo -y apt-get -f install
-    echo "--- Fim da instalação do visual studio code  ---"  
-
-
-elif [ "$programas" = "5" ];
 then
     echo "--- instalando Chrome ---"
     sleep 3      
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     sudo -y apt-get -f install
-    
+    echo "--- Fim da instalação do Chrome  ---" 
 fi
 
 ####LOOP E VOLTA AO MENU#####
